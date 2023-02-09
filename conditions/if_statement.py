d4d40a44488df992c9e8176512296da34f21781e
eMobilis <elikibetael4@gmail.com>
2023-02-09 09:39:49 +0300

    if statements and data structures in python

--- conditions/if_statement.py
+++ conditions/if_statement.py
@@ -1,13 +1,13 @@
 fszd = int(input("Enter a number"))
-if fszd == 1 :
+if fszd == 1:
     print("Enroll for bonga points")
-elif fszd== 2 :
+elif fszd == 2:
     print("Data deals")
-elif fszd== 3 :
+elif fszd == 3:
     print("Daily Bundles")
-elif fszd== 4 :
+elif fszd == 4:
     print("Weekly Bundles")
-elif fszd== 5 :
+elif fszd == 5:
     print("No Expiry")
 else:
     print("Thank you")
