aa39d598b5eb13c11738c4a080b9bdb75b9c3b4d
eMobilis <elikibetael4@gmail.com>
2023-02-13 10:20:48 +0300

    intro to python

---
+++ main.py
@@ -0,0 +1,11 @@
+num1 =447
+num2 =553
+
+result =num1+num2
+
+print(result)
+num4 =125
+num5 =40
+
+final =num4*num5
+print(final)
---
+++ tulei.py
@@ -0,0 +1,5 @@
+school ="emobilis"
+print(school)
+x = "Hello"
+y = " world"
+print(x+y)
